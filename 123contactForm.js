43652b552aab745dd4f2b2bb44e5ead3e144e7f9
RoisinOk <roisin_ok@hotmail.com>
2016-03-25 20:06:12 +0000

    Adding contact form.

---
+++ 123contactForm.js
@@ -0,0 +1,76 @@
+/**
+ * Created by roisinokeeffe on 25/03/16.
+ */
+
+
+var cfJsHost = (("https:" == document.location.protocol) ? "https://" : "http://");
+document.write(unescape("%3Cscript src='" + cfJsHost + "dflzqrzibliy5.cloudfront.net/modules/calculations/form/js/form.calculations.js.minified.js?c3feee4' type='text/javascript'%3E%3C/script%3E"));
+
+
+
+var tr_parents=[];
+var formrules=new Array(0);
+
+
+
+var cfJsHost = (("https:" == document.location.protocol) ? "https://" : "http://");
+document.write(unescape("%3Cscript src='" + cfJsHost + "dflzqrzibliy5.cloudfront.net/includes/interactive123cf.js.minified.js?c3feee4' type='text/javascript'%3E%3C/script%3E"));
+
+var cfJsHost = (("https:" == document.location.protocol) ? "https://" : "http://");
+document.write(unescape("%3Cscript src='" + cfJsHost + "www.123contactform.com/libraries/select2/select2.min.js.minified.js' type='text/javascript'%3E%3C/script%3E"));
+
+var cfJsHost = (("https:" == document.location.protocol) ? "https://" : "http://");
+document.write(unescape("%3Cscript src='" + cfJsHost + "www.123contactform.com/libraries/select2/select2-simple.js?c3feee4' type='text/javascript'%3E%3C/script%3E"));
+
+var cfJsHost = (("https:" == document.location.protocol) ? "https://" : "http://");
+document.write(unescape("%3Cscript src='" + cfJsHost + "www.123contactform.com/libraries/datepicker/bootstrap-datepicker.js.minified.js' type='text/javascript'%3E%3C/script%3E"));
+
+var cfJsHost = (("https:" == document.location.protocol) ? "https://" : "http://");
+document.write(unescape("%3Cscript src='" + cfJsHost + "dflzqrzibliy5.cloudfront.net/modules/weebly/js/weebly.js.minified.js?c3feee4' type='text/javascript'%3E%3C/script%3E"));
+
+var fid=1884322;
+var curr='%26%2336%3B';
+var preview_iframe=0;
+var real_time=0;
+
+var selectedfield='';
+var multiPageCurent=1;
+var multiPageTotal=1;
+function InputActions(field,id) {
+    window.stopCalculate=true;
+    var textcolor='';
+    var hltextcolor='#4C4C4C';
+    $('.class123-labelhidden:not(.hidden_instruction)').addClass('hidden_instruction');
+    if (!(($(field).prop('tagName') =='INPUT') && (($(field).prop('type')=="text") || ($(field).prop('type')=="password")) ))
+        if ($(field).prop('tagName') !='TEXTAREA')
+            remove_field_error(field);
+
+    $(field).parents('.fieldcontainer').last().find('.hidden_instruction').removeClass('hidden_instruction');
+    //var calendar = $('img[title="Close Calendar"]').trigger('click');
+
+    lastactionobj=field;
+
+    if (id==selectedfield) { /* alert('do nothing'); */	}
+
+    // now the field rules
+
+    InputRules2('beginning','0','0');
+}
+
+
+var  js_ctype_arr = new Array();
+var  js_cobject_arr = new Array();
+js_ctype_arr['cid18122328']=0;
+js_cobject_arr['cid18122328']=0;
+js_ctype_arr['cid18122329']=0;
+js_cobject_arr['cid18122329']=0;
+js_ctype_arr['cid18122330']=1;
+js_cobject_arr['cid18122330']=0;
+
+var f_fixedamount='0';
+var nr_fields=3;
+this.nr_fields=nr_fields;
+this.form_is_quiz='';
+var calc_fields=0;
+
+InputRules('firsttime');
